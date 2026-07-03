e8c6049467a44f2aecfafd0e543826fc05266920
momo <ptjkjm1@gmail.com>
2026-07-03 14:27:54 +0900

    Initial commit

---
+++ src/main/java/io/github/prjkmo112/schedulerappdevelop/SchedulerAppDevelopApplication.java
@@ -0,0 +1,13 @@
+package io.github.prjkmo112.schedulerappdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerAppDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerAppDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/prjkmo112/schedulerappdevelop/SchedulerAppDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.prjkmo112.schedulerappdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerAppDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
