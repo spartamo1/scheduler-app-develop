edd397878e696eda83e58974eab425faa1676d69
momo <ptjkjm1@gmail.com>
2026-07-05 02:55:08 +0900

    feat: exception 설정 추가

---
+++ src/main/java/io/github/prjkmo112/schedulerappdevelop/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package io.github.prjkmo112.schedulerappdevelop.exception;
+
+import java.util.Map;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<Map<String, String>> handleApiException(ApiException e) {
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(Map.of("message", e.getMessage()));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Map<String, String>> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Map.of("message", "Data integrity constraint violation"));
+    }
+
+}
