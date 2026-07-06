539c552e257f5878ffe161a9065ee3291de7ff8b
momo <ptjkjm1@gmail.com>
2026-07-06 21:07:16 +0900

    feat: 요청 데이터 검증 실패에 대한 예외 처리 로직 추가

--- src/main/java/io/github/prjkmo112/schedulerappdevelop/domain/schedule/dto/CreateScheduleReqDto.java
+++ src/main/java/io/github/prjkmo112/schedulerappdevelop/domain/schedule/dto/CreateScheduleReqDto.java
@@ -13,11 +13,11 @@ import lombok.Setter;
 public class CreateScheduleReqDto {
 
     @NotBlank
-    @Size(max = 30)
+    @Size(max = 10, message = "제목은 10자 이하로 입력해주세요")
     private String title;
 
     @NotBlank
-    @Size(max = 200)
+    @Size(max = 200, message = "내용은 200자 이하로 입력해주세요")
     private String content;
 
 }
--- src/main/java/io/github/prjkmo112/schedulerappdevelop/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/prjkmo112/schedulerappdevelop/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,6 +24,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(Map.of("message", "Data integrity constraint violation"));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("message", "Validation failed: " + e.getBindingResult().getFieldError().getDefaultMessage()));
     }
 
 }
