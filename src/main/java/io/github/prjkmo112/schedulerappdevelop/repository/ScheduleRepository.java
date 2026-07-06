0f68a507943e1901984da8d98fe43d3700b5a5f6
momo <ptjkjm1@gmail.com>
2026-07-06 19:48:08 +0900

    refactor: ScheduleRepository에서 불필요한 메서드 제거

--- src/main/java/io/github/prjkmo112/schedulerappdevelop/repository/ScheduleRepository.java
+++ src/main/java/io/github/prjkmo112/schedulerappdevelop/repository/ScheduleRepository.java
@@ -1,16 +1,8 @@
 package io.github.prjkmo112.schedulerappdevelop.repository;
 
-import java.util.List;
-
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import io.github.prjkmo112.schedulerappdevelop.entity.Schedule;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-
-    @Query("SELECT s FROM Schedule s LEFT JOIN FETCH s.user")
-    List<Schedule> findAll(Sort sort);
-
 }
